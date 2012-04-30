3e3165065606b14d0db3579ec6e2e5915f275763
Andreas Bock <bock@andreasbock.dk>
2012-04-30 16:45:32 +0200

    Inserted while loop for handle_args in ebprof.c. Rest after dinnerz

--- kernel/ebprof.c
+++ kernel/ebprof.c
@@ -14,7 +14,14 @@ int buf_size = 0;
 extern int *pri_buf;
 extern int *sec_buf;
 
-/* Initializes datastructures used for profiling */
+/* Starts event-based profiling. */
+int
+main(int argc, char* argv[])
+{
+	handle_args(argc, argv);
+}
+
+/* Initializes datastructures used for profiling. */
 int
 start()
 {
@@ -32,4 +39,51 @@ collect (message *m_user, struct proc *caller)
 	return 0;
 }
 
-#endif /* EBPROFILE */
+/* Handle arguments */
+int
+handle_args(int argc, char* argv[])
+{
+
+while (--argc) {
+        ++argv;
+
+        if (strcmp(*argv, "-h") == 0 || strcmp(*argv, "help") == 0 ||
+                strcmp(*argv, "--help") == 0) {
+                return EHELP;
+        } else
+        if (strcmp(*argv, "-m") == 0) {
+                if (--argc == 0) return ESYNTAX;
+                if (sscanf(*++argv, "%u", &mem_size) != 1 ||
+                        mem_size < MIN_MEMSIZE ) return EMEM;
+        } else
+        if (strcmp(*argv, "-f") == 0) {
+                if (--argc == 0) return ESYNTAX;
+                if (sscanf(*++argv, "%u", &freq) != 1)
+                        return EFREQ;
+        } else
+        if (strcmp(*argv, "-o") == 0) {
+                if (--argc == 0) return ESYNTAX;
+                outfile = *++argv;
+        } else
+        if (strcmp(*argv, "--rtc") == 0) {
+                intr_type = PROF_RTC;
+        } else
+        if (strcmp(*argv, "--nmi") == 0) {
+                intr_type = PROF_NMI;
+        } else
+        if (strcmp(*argv, "start") == 0) {
+                if (action) return EACTION;
+                action = START;
+        } else
+        if (strcmp(*argv, "stop") == 0) {
+                if (action) return EACTION;
+                action = STOP;
+        } else
+        if (strcmp(*argv, "get") == 0) {
+                if (action) return EACTION;
+                action = GET;
+}
+
+}
+
+#endif
