e9fdea0441049c1247f2b40542284bf51caf0984
Andreas Bock <bock@andreasbock.dk>
2012-04-30 10:48:11 +0200

    memory allocation for buffers

--- kernel/ebprof.c
+++ kernel/ebprof.c
@@ -1,1 +1,33 @@
-#include 
+/* This file implements event-based profiling
+ * in MINIX 3. 
+*/
+
+#include "ebprof.h"
+
+
+int alloc_buffers (void);
+
+int *pri_buf;
+int *sec_buf;
+
+
+/* Allocates memory for double buffering */
+int
+alloc_buffers ()
+{
+  pri_buf = malloc (BUFFER_SIZE);
+  sec_buf = malloc (BUFFER_SIZE);
+
+  if (pri_buf == 0 || sec_buf == 0)
+    {
+      printf
+	("Could not allocate buffers. Disabling event-based profiling.\n")
+	return 1;
+    }
+  else
+    {
+      memset (pri_buf, '\0', BUFFER_SIZE);
+      memset (sec_buf, '\0', BUFFER_SIZE);
+    }
+
+}
--- kernel/ebprof.h
+++ kernel/ebprof.h
@@ -1,7 +1,11 @@
 #ifndef EBPROF_H
 #define EBPROF_H
 
+/* This header file defines ...
+*/
+
 #if EBPROFILE
 
+#define BUFFER_SIZE 4096
 
 #endif /* EBPROF_H */
