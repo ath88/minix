861de2550986d8f2e4e214957eaf7be40cf395ca
Andreas Bock <bock@andreasbock.dk>
2012-05-03 14:43:17 +0200

    added kernel functionality (buffer mgmt etc.)

---
+++ kernel/ebp_kernel.c
@@ -0,0 +1,23 @@
+/* Kernel functions for event-based profiling
+*/
+
+
+#include "ebp.h"
+
+#if EBPROFILE
+
+/* Returns pointer to active buffer */
+int*
+active_buffer()
+{
+
+}
+
+/* Returns pointer to inactive buffer */
+int*
+inactive_buffer()
+{
+
+}
+
+#endif /* EBPROFILE */
